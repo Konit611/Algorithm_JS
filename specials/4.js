81ce025e6c64f24830505fea8d5304dd870d358f
Konit611 <konit611@gmail.com>
2026-04-10 09:11:14 +0900

    TIL 20260410

--- specials/4.js
+++ specials/4.js
@@ -1,6 +1,31 @@
-// 다리를 지나가는 트럭
+// 기능 개발
 
-function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
+function solution(progresses, speeds) {
+    const answer = [];
+
+    let pointer = 0;
+    while (progresses.length > pointer) {
+        speeds.forEach((speedForEachProcess, i) => {
+            if (progresses[i] <= 100) {
+                progresses[i] += speedForEachProcess;
+            }
+        });
+
+        let proceed_count = 0;
+        while (progresses[pointer] >= 100) {
+            proceed_count++;
+            pointer++;
+        }
+
+        if (proceed_count > 0) {
+            answer.push(proceed_count);
+        }
+    }    
+
     return answer;
 }
+
+const progresses = [95, 90, 99, 99, 80, 99];
+const speeds = [1, 1, 1, 1, 1, 1];
+
+console.log(solution(progresses, speeds));
